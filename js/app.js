95e905de82629bb6e51c2eeee4a09a92fff1a515
Andres De Leon <agldeleon@gmail.com>
2013-02-23 14:08:29 -0600

    Agregue un vergo de cosas

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
 $(function(){
 
-  updateData = function(data){
-	  console.log(data);
-  }
+  updateData = function(tweets){
+      user = Helpers.getUser(tweets);
+      displayData();
+  }
+
+  displayData = function(data) {
+    Display.userInfo(user);
+  }
 
 
-  pullDataFromTwitter = function(){
-    var user = $("#user_name").val();
-    var url = "http://api.twitter.com/1/statuses/user_timeline.json";
+  pullDataFromTwitter = function(){
+    var user = $("#user_name").val();
+    var url = "http://api.twitter.com/1/statuses/user_timeline.json";
 
-    $.ajax({
-      url: url,
-      dataType: "jsonp",
-      data: {count: 50, include_entities: true, screen_name: user},
-      success: updateData,
-      error: function(){ console.log("NO");}
-    });
-  }
+    $.ajax({
+      url: url,
+      dataType: "jsonp",
+      data: {count: 50, include_entities: true, screen_name: user},
+      success: updateData,
+      error: function(){ console.log("NO");}
+    });
+  }
 
 
-  pullDataFromTwitter();
+  pullDataFromTwitter();
 
-  $("#search").on("click",function(event){
-    event.preventDefault();
-    pullDataFromTwitter();
-  });
+  $("#search").on("click",function(event){
+    event.preventDefault();
+    pullDataFromTwitter();
+  });
 });
